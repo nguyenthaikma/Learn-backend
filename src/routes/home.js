87ea43e674cd92c663770949030f1f89681c2dde
Nguyen <thainh@twinger.vn>
2024-05-21 14:53:31 +0700

    rm base

--- src/routes/home.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getHome, deleteUserById } = require("../controllers/homeController");
-const router = express.Router();
-
-router.get("/", getHome);
-
-module.exports = router;
--- src/routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { postRegister, getRegister } = require("../controllers/registerController");
-const router = express.Router();
-
-router.post("/action-register", postRegister);
-router.get("/", getRegister);
-
-module.exports = router;
